8614343ca8f6d311808222aa2ba71fc6c9f8e9d3
decoricky <rikiya.shikida@gmail.com>
2022-03-23 12:19:58 +0900

    vscodeのsettings.jsonを管理下に追加

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/vim/coc-settings.json ~/.config/nvim/coc-settings.json
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
+ln -sf ~/dotfiles/vscode/settings.json ~/Library/ApplicationSupport/Code/User/settings.json
