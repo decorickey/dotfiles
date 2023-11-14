cd13480bd0414c2127c1f86c62af23d19351ae07
decorickey <rikiya.shikida@gmail.com>
2023-11-14 10:56:08 +0900

    refactor

--- init.lua
+++ init.lua
@@ -3,15 +3,20 @@ if vim.g.vscode then
   return {
     vim.cmd("source ~/dotfiles/.vimrc"),
 
+    -- ターミナル
+    vim.keymap.set(
+      { "n" }, "<leader>th", "workbench.action.terminal.new", { noremap = true }
+    ),
+
     -- コメント
     vim.keymap.set(
-      { "n", "v" }, "<leader>/", "<Cmd>call VSCodeNotify('editor.action.commentLine')<CR>", { noremap = true }
+      { "n" }, "<leader>/", "<Cmd>call VSCodeNotify('editor.action.commentLine')<CR>", { noremap = true }
     ),
 
     -- fold
     vim.keymap.set({ "n" }, "zf", "<Cmd>call VSCodeNotify('editor.fold')<CR>", { noremap = true }),
     vim.keymap.set({ "n" }, "zd", "<Cmd>call VSCodeNotify('editor.unfold')<CR>", { noremap = true }),
-    -- （カーソル移動時にfoldをスキップ）
+    -- カーソル移動時にfoldをスキップ（VSCodeの挙動変更）
     vim.keymap.set(
       { "n" }, "j",
       "<Cmd>call VSCodeCall('cursorMove', { 'to': 'down', 'by': 'wrappedLine', 'value': v:count ? v:count : 1 })<CR>",
@@ -24,16 +29,23 @@ if vim.g.vscode then
     ),
 
     -- Telescope
-    vim.keymap.set({ "n" }, "<leader>ff", "<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>", { noremap = true }),
+    vim.keymap.set(
+      { "n" }, "<leader>ff", "<Cmd>call VSCodeNotify('workbench.action.quickOpen')<CR>", { noremap = true }
+    ),
     vim.keymap.set(
       { "n" }, "<leader>fw", "<Cmd>call VSCodeNotify('workbench.action.findInFiles')<CR>", { noremap = true }
+    ),
+    vim.keymap.set(
+      { "n" }, "<leader>fo", "<Cmd>call VSCodeNotify('workbench.action.findInFiles')<CR>", { noremap = true }
     ),
 
     -- Neo-Tree
     vim.keymap.set(
       { "n" }, "<leader>e", "<Cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<CR>", { noremap = true }
     ),
-    vim.keymap.set({ "n" }, "<leader>o", "<Cmd>call VSCodeNotify('workbench.view.explorer')<CR>", { noremap = true }),
+    vim.keymap.set(
+      { "n" }, "<leader>o", "<Cmd>call VSCodeNotify('workbench.view.explorer')<CR>", { noremap = true }
+    ),
   }
 else
   -- ordinary Neovim
