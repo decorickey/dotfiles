b0945e5e0109b9061fed2d3f5d3a1195f3af8478
decorickey <rikiya.shikida@gmail.com>
2024-02-29 17:14:49 +0900

    fix shell

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,11 +4,13 @@ ln -sf ~/dotfiles/.vimrc ~/.ideavimrc
 DIR="${HOME}/.config/nvim/lua/user"
 if [ ! -d ${DIR} ]; then
   mkdir ${DIR}
+  echo "Created \"${DIR}\""
 fi
 ln -sf ~/dotfiles/nvim/init.lua "${DIR}/init.lua"
 ln -sf ~/dotfiles/nvim/mappings.lua "${DIR}/mappings.lua"
 ln -sf ~/dotfiles/nvim/options.lua "${DIR}/options.lua"
 ln -sf ~/dotfiles/nvim/plugins "${DIR}/plugins"
+echo "Created symbolic links"
 
 DIR="${HOME}/.ipython/profile_default"
 if [ -d ${DIR} ]; then
@@ -18,3 +20,4 @@ else
   echo "Exec \"ipython profile create\""
 fi
 
+echo "Complete!!!"
