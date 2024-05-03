a1b98569321800535c6cb63b03a226c12ce0b80e
decorickey <rikiya.shikida@gmail.com>
2024-05-03 22:21:53 +0900

    bugfix

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,5 +2,5 @@
 ln -sf ~/dotfiles/.vimrc ~/.ideavimrc
 echo "created symbolic link for IdeaVim"
 
-ln -sf ~/dotfiles/astronvim_config ~/.config/nvim
+ln -sf ~/dotfiles/nvim ~/.config/nvim
 echo "created symbolic link for AstroNvim"
