1dcdbe531b4431ab6eea3852dec1d137cbe5807f
decorickey <rikiya.shikida@gmail.com>
2024-05-07 17:59:01 +0900

    remove unchanged files

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,6 +2,7 @@
 ln -sf ~/dotfiles/.vimrc ~/.ideavimrc
 echo "created symbolic link for IdeaVim"
 
-rm -rf ~/.config/nvim
-ln -sf ~/dotfiles/astronvim_config ~/.config/nvim
+ln -sf ~/dotfiles/astronvim_config/lua/plugins/astrocore.lua ~/.config/nvim/lua/plugins/astrocore.lua
+ln -sf ~/dotfiles/astronvim_config/lua/plugins/astrolsp.lua ~/.config/nvim/lua/plugins/astrolsp.lua
+ln -sf ~/dotfiles/astronvim_config/lua/plugins/mason.lua ~/.config/nvim/lua/plugins/mason.lua
 echo "created symbolic link for AstroNvim"
