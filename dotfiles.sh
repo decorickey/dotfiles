08753775f568f7fe364a53697ed85b499c9992d3
decorickey <rikiya.shikida@gmail.com>
2024-06-18 22:13:36 +0900

    add vim-go

--- dotfiles.sh
+++ dotfiles.sh
@@ -6,4 +6,5 @@ ln -sf ~/dotfiles/astronvim_config/lua/plugins/astrocore.lua ~/.config/nvim/lua/
 ln -sf ~/dotfiles/astronvim_config/lua/plugins/astrolsp.lua ~/.config/nvim/lua/plugins/astrolsp.lua
 ln -sf ~/dotfiles/astronvim_config/lua/plugins/mason.lua ~/.config/nvim/lua/plugins/mason.lua
 ln -sf ~/dotfiles/astronvim_config/lua/plugins/user.lua ~/.config/nvim/lua/plugins/user.lua
+ln -sf ~/dotfiles/astronvim_config/lua/plugins/go.lua ~/.config/nvim/lua/plugins/go.lua
 echo "created symbolic link for AstroNvim"
