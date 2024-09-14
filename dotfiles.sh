7c723643262a74a39604766b1a29e862f3dcb0e9
decorickey <rikiya.shikida@gmail.com>
2024-09-14 20:13:57 +0900

    link

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,25 +1,26 @@
 #!/bin/zsh
 
-# NeoVim設定
-NVIM_DIR=~/.config/nvim
-
 # 引数が渡されているかをチェック
 if [ -z "$1" ]; then
   echo "エラー: 引数が必要です (lazy または astro)"
   exit 1
 fi
 
+# NeoVim設定
+CONFIG_DIR=~/.config
+NVIM_DIR=~/.config/nvim
+
 # 既存のNeovim設定を削除
-rm -rf $NVIM_DIR
-mkdir -p $NVIM_DIR
+rm -rf $CONFIG_DIR
+mkdir -p $CONFIG_DIR
 
 # 引数に応じて処理を分岐
 if [ "$1" = "lazy" ]; then
-  cp -rf ~/dotfiles/lazynvim/* $NVIM_DIR
-  echo "LazyVim 設定をコピーしました。"
+  ln -s ~/dotfiles/lazynvim $NVIM_DIR
+  echo "LazyVim を設定しました。"
 elif [ "$1" = "astro" ]; then
-  cp -rf ~/dotfiles/astronvim/* $NVIM_DIR
-  echo "AstroNvim 設定をコピーしました。"
+  ln -s ~/dotfiles/astronvim $NVIM_DIR
+  echo "AstroNvim を設定しました。"
 else
   echo "エラー: 不正な引数です。'lazy' または 'astro' を指定してください。"
   exit 1
