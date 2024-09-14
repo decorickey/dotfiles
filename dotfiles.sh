117e09a6999f3003a8da6d29c457941f5fb831bb
decorickey <rikiya.shikida@gmail.com>
2024-09-14 17:10:48 +0900

    fix bug

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,6 +5,6 @@ echo "setup IdeaVim"
 NVIM_DIR=~/.config/nvim
 rm -rf ~/.config/nvim
 mkdir -p $NVIM_DIR
-cp -rf ~/dotfiles/lazynvim/* $NVIM_DIR
-# cp -rf ~/dofiles/astronvim/* $NVIM_DIR
+# cp -rf ~/dotfiles/lazynvim/* $NVIM_DIR
+cp -rf ~/dotfiles/astronvim/* $NVIM_DIR
 echo "setup NeoVim"
