4ab589616c0ca6879090a6343fe7c9f1ea4c5185
decorickey <rikiya.shikida@gmail.com>
2024-09-14 16:28:35 +0900

    refactor

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,9 +2,9 @@
 ln -sf ~/dotfiles/.vimrc ~/.ideavimrc
 echo "created symbolic link for IdeaVim"
 
-ln -sf ~/dotfiles/astronvim_config/lua/plugins/astrocore.lua ~/.config/nvim/lua/plugins/astrocore.lua
-ln -sf ~/dotfiles/astronvim_config/lua/plugins/astrolsp.lua ~/.config/nvim/lua/plugins/astrolsp.lua
-ln -sf ~/dotfiles/astronvim_config/lua/plugins/mason.lua ~/.config/nvim/lua/plugins/mason.lua
-ln -sf ~/dotfiles/astronvim_config/lua/plugins/user.lua ~/.config/nvim/lua/plugins/user.lua
-ln -sf ~/dotfiles/astronvim_config/lua/plugins/go.lua ~/.config/nvim/lua/plugins/go.lua
+ln -sf ~/dotfiles/astronvim/lua/plugins/astrocore.lua ~/.config/nvim/lua/plugins/astrocore.lua
+ln -sf ~/dotfiles/astronvim/lua/plugins/astrolsp.lua ~/.config/nvim/lua/plugins/astrolsp.lua
+ln -sf ~/dotfiles/astronvim/lua/plugins/mason.lua ~/.config/nvim/lua/plugins/mason.lua
+ln -sf ~/dotfiles/astronvim/lua/plugins/user.lua ~/.config/nvim/lua/plugins/user.lua
+ln -sf ~/dotfiles/astronvim/lua/plugins/go.lua ~/.config/nvim/lua/plugins/go.lua
 echo "created symbolic link for AstroNvim"
