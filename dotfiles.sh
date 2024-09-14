19767225790bbc40cd7b460e93e8dd3b2295cf25
decorickey <rikiya.shikida@gmail.com>
2024-09-14 17:13:33 +0900

    fix

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,7 +3,7 @@ ln -sf ~/dotfiles/.vimrc ~/.ideavimrc
 echo "setup IdeaVim"
 
 NVIM_DIR=~/.config/nvim
-rm -rf ~/.config/nvim
+rm -rf $NVIM_DIR
 mkdir -p $NVIM_DIR
 # cp -rf ~/dotfiles/lazynvim/* $NVIM_DIR
 cp -rf ~/dotfiles/astronvim/* $NVIM_DIR
