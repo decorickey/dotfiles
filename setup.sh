ee2737b158e247620a56849708a16dcaf1bf600e
decorickey <43911600+decorickey@users.noreply.github.com>
2025-01-18 11:08:43 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 if ! which brew &>/dev/null; then
   echo "Install Homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo >>/Users/decorickey/.zprofile
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>/Users/decorickey/.zprofile
+  echo >>~/.zprofile
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>~/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   echo "Update Homebrew"
