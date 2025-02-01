6da0cf11d6a80fda6718b64f8288652411701fa7
decorickey <rikiya.shikida@gmail.com>
2025-02-01 15:17:16 +0900

    Update

--- lazyvim/lua/plugins/treesitter.lua
+++ lazyvim/lua/plugins/treesitter.lua
@@ -1,7 +1,12 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	opts = {
-		ensure_installed = {
-		},
-	},
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    highlight = { enable = true },
+    indent = { enable = true },
+    ensure_installed = {
+      "lua",
+      "markdown",
+      "markdown_inline",
+    },
+  },
 }
