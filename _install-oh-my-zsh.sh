112b3d1417fd189ce2f1d63f1e0b7995910041f4
decorickey <43911600+decorickey@users.noreply.github.com>
2025-02-21 09:39:21 +0900

    Update _install-oh-my-zsh.sh

--- _install-oh-my-zsh.sh
+++ _install-oh-my-zsh.sh
@@ -3,10 +3,16 @@
 if ! which zsh &>/dev/null; then
   echo "***** Install zsh *****"
   $INSTALL_CMD zsh
-  chsh -s $(which zsh)
-  echo "Restart Terminal"
 else
   echo "***** Skip zsh *****"
+fi
+echo
+
+DEFAULT_SHELL=$(getent passwd "$USER" | cut -d: -f7)
+ZSH_PATH=$(which zsh)
+if [ "$DEFAULT_SHELL" != "$ZSH_PATH" ]; then
+    echo "***** Change Default Shell to Zsh *****"
+    chsh -s "$ZSH_PATH"
 fi
 echo
 
