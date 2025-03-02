15e03975586418982ad5941286bf1202f14f4387
decorickey <rikiya.shikida@gmail.com>
2025-03-02 13:29:16 +0900

    Update

--- lazyvim/lua/plugins/render-markdown.lua
+++ lazyvim/lua/plugins/render-markdown.lua
@@ -1,5 +1,10 @@
 return {
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-	},
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      heading = {
+        enabled = false,
+      },
+    },
+  },
 }
