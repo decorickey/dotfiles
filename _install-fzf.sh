9cac1f9f60aba58532f3a303fb422b06055c15db
decorickey <43911600+decorickey@users.noreply.github.com>
2025-03-13 14:39:45 +0900

    Update _install-fzf.sh

--- _install-fzf.sh
+++ _install-fzf.sh
@@ -4,13 +4,13 @@ if ! [ -d ~/.fzf ]; then
   echo "***** Install fzf *****"
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   cd ~/.fzf
-  ./install
+  ./install --all
   cd ~/dotfiles
 else
   echo "***** Update fzf *****"
   cd ~/.fzf
   git pull
-  ./install
+  ./install --all
   cd ~/dotfiles
 fi
 echo
