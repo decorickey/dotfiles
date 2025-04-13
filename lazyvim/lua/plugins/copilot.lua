e8e48c18883fd3f6a292f027ec26c6c19ecc049f
decorickey <rikiya.shikida@gmail.com>
2025-04-13 21:06:38 +0900

    Update

--- lazyvim/lua/plugins/copilot.lua
+++ lazyvim/lua/plugins/copilot.lua
@@ -1,3 +1,12 @@
 return {
   "github/copilot.vim",
+  init = function()
+    -- TabをCopilotに使わせない
+    vim.g.copilot_no_tab_map = true
+
+    -- Ctr-L で補完を承認
+    vim.cmd([[
+      imap <silent><script><expr> <C-l> copilot#Accept("\<CR>")
+    ]])
+  end,
 }
