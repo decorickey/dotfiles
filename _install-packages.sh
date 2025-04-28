b688fd2ddfb2494f706cce1d0a5a771b5fdb0209
decorickey <rikiya.shikida@gmail.com>
2025-04-28 21:47:50 +0900

    Update

--- _install-packages.sh
+++ _install-packages.sh
@@ -3,9 +3,18 @@
 if ! which brew &>/dev/null; then
   echo "***** homebrew package manager not found, installing... *****"
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo >>~/.zprofile
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>~/.zprofile
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+
+  if [[ "$(uname)" == "Darwin" ]]; then
+    echo >>~/.zshrc
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>~/.zshrc
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  elif [[ "$(uname)" == "Linux" ]]; then
+    echo >>~/.bashrc
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>~/.bashrc
+    echo >>~/.zshrc
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>~/.zshrc
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  fi
 else
   echo "***** homebrew package manager found, updating... *****"
   brew update
