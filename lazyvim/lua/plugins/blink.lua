dbba1410ad5e441034e4f17b6c39871483212d25
decorickey <rikiya.shikida@gmail.com>
2025-07-26 18:30:05 +0900

    update

--- lazyvim/lua/plugins/blink.lua
+++ lazyvim/lua/plugins/blink.lua
@@ -1,9 +1,13 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    completion = { -- list = {
-      --   selection = { preselect = false, auto_insert = false },
-      -- },
+    completion = {
+      list = {
+        selection = {
+          preselect = false,
+          -- auto_insert = false,
+        },
+      },
     },
     keymap = {
       ["<CR>"] = { "accept", "fallback" },
