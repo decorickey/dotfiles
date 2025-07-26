a879e9e4cb7c85f9699492e19a9e315eedf63820
decorickey <rikiya.shikida@gmail.com>
2025-07-26 15:56:20 +0900

    update

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -2,10 +2,3 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.cmd("source ~/dotfiles/.vimrc")
-
-if vim.g.vscode then
-  local vscode = require("vscode")
-  vim.keymap.set("n", "<leader>e", function()
-    vscode.action("workbench.view.explorer")
-  end, { remap = true })
-end
