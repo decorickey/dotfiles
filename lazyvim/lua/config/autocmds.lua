70388d346f669b81c9d1bdeb80a34e383de397d5
decorickey <rikiya.shikida@gmail.com>
2025-08-03 12:42:49 +0900

    update

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -2,9 +2,8 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+  pattern = { "markdown" },
   callback = function()
-    vim.opt_local.spell = false
     vim.b.autoformat = false
   end,
 })
