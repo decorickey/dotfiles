fdfb1614594e08851e98d55f21a9da79a4259419
decorickey <rikiya.shikida@gmail.com>
2025-08-03 13:19:50 +0900

    update

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -1,9 +1,20 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Disable autoformat and spell checking for markdown files
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "markdown" },
   callback = function()
     vim.b.autoformat = false
+    vim.opt_local.spell = false
+  end,
+})
+
+-- Disable spell checking for gitcommit files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "gitcommit" },
+  callback = function()
+    vim.opt_local.spell = false
   end,
 })
