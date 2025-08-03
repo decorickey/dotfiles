5c2eb1cf06ed510d596d277e500357edb8a1fb49
decorickey <rikiya.shikida@gmail.com>
2025-08-03 12:36:15 +0900

    update

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -5,5 +5,6 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
   callback = function()
     vim.opt_local.spell = false
+    vim.b.autoformat = false
   end,
 })
