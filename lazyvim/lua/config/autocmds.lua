992684207914436da2b673fc2bc93988dde59d66
decorickey <rikiya.shikida@gmail.com>
2025-10-25 13:14:48 +0900

    fix

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -1,21 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
 -- Add any additional autocmds here
-
--- Disable autoformat and spell checking for markdown files
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "markdown", "sh" },
-  callback = function()
-    vim.b.autoformat = false
-    vim.opt_local.spell = false
-  end,
-})
-
--- Disable spell checking for gitcommit files
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "gitcommit" },
-  callback = function()
-    vim.opt_local.spell = false
-  end,
-})
-
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
