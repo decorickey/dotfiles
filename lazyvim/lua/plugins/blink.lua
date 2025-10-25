9d07661c19e38088cc61b4b4bbb8864f78e4fab7
decorickey <rikiya.shikida@gmail.com>
2025-10-25 20:20:15 +0900

    fix

---
+++ lazyvim/lua/plugins/blink.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        list = {
+          selection = {
+            -- 自動補完候補がデフォルトで選択されないようにするための設定
+            preselect = false,
+            auto_insert = false,
+          },
+        },
+      },
+    },
+  },
+}
