a3c0ef64782b1c265a2a71bca2c3021bf698c612
decorickey <rikiya.shikida@gmail.com>
2025-10-25 14:51:55 +0900

    fix

--- lazyvim/lua/plugins/lint.lua
+++ lazyvim/lua/plugins/lint.lua
@@ -1,9 +1,6 @@
 return {
   "mfussenegger/nvim-lint",
   opts = {
-    linters_by_ft = {
-      markdown = { "markdownlint-cli2" },
-    },
     linters = {
       ["markdownlint-cli2"] = {
         args = { "--config", vim.fn.expand("~/dotfiles/.markdownlint-cli2.jsonc"), "--" },
