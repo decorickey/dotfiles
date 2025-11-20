d950b6b95ce54525de4b23573eace3c0d6b6adfe
decorickey <rikiya.shikida@gmail.com>
2025-11-20 10:43:28 +0900

    fix

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>/", "<leader>sG", {
+  remap = true,
+  silent = true,
+  desc = "Grep (cwd)",
+})
+
+vim.keymap.set("n", "<leader>e", "<leader>E", {
+  remap = true,
+  silent = true,
+  desc = "Explorer (cwd)",
+})
+
+vim.keymap.set("n", "<leader>ff", "<leader>fF", {
+  remap = true,
+  silent = true,
+  desc = "Find Files (cwd)",
+})
