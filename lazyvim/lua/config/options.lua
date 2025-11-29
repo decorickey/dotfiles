fcdffb9636995eb3150727a3bfc6e70d4c5acbeb
decorickey <rikiya.shikida@gmail.com>
2025-11-29 10:48:39 +0900

    fix

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -3,3 +3,9 @@
 -- Add any additional options here
 
 vim.cmd("source ~/dotfiles/.vimrc")
+
+-- foldはデフォルトで展開した状態にし、必要に応じて手動で切り替えられるようにする
+local opt = vim.opt
+opt.foldenable = false
+opt.foldlevel = 99
+opt.foldlevelstart = 99
